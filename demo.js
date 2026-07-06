7673fc5d31d4c7bf78075429559032f97c71d43c
Aimable Ndizeye <naimable883@gmail.com>
2026-07-06 19:20:35 +0200

    Crucial patterns

--- demo.js
+++ demo.js
@@ -36,28 +36,61 @@
 // console.log( checkPassed ([39,80,40,78,34]))
 
 
-const person = {
-    name: "aimable",
-    age : 24,
-    status : "single"
+// const person = {
+//     name: "aimable",
+//     age : 24,
+//     status : "single"
     
+// }
+
+// const propertyi = Object.entries(person)
+// console.log(propertyi)
+
+// const arr = [2,5,7,4,6,3]
+// const newArr = [...arr , 4]
+
+// console.log(newArr);
+
+
+// const multipleArr = [
+//     ["name", "Aimable"] 
+//     ,["age" , 45]
+// ]
+
+// const obj = Object.fromEntries(multipleArr)
+
+// console.log(obj);
+
+
+
+
+
+
+const letters = ["banana","banana", "orange", "Fruits"];
+
+const freq = {};
+
+for (const letter of letters) 
+    // freq[letter] = (freq[letter] || 0) + 1;
+{
+    for (const str of letter) {
+
+    if (freq[str]){
+        freq[str] = freq[str] + 1
+    
+    }
+    else {
+        freq[str] = 1;
+    }
+
+    }
+   
 }
+const sorted = Object.keys(freq).sort().reduce((acc,key) => {
+    acc[key] = freq[key];
+    return acc;
+}, {});
 
-const propertyi = Object.entries(person)
-console.log(propertyi)
+ console.log(sorted);
 
-const arr = [2,5,7,4,6,3]
-const newArr = [...arr , 4]
-
-console.log(newArr);
-
-
-const multipleArr = [
-    ["name", "Aimable"] 
-    ,["age" , 45]
-]
-
-const obj = Object.fromEntries(multipleArr)
-
-console.log(obj);
 
