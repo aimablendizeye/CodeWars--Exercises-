83e03a44b0cb80468a3578f6df4610d4d1a715b3
Aimable Ndizeye <naimable883@gmail.com>
2026-07-08 22:32:30 +0200

    Q16

--- script.js
+++ script.js
@@ -495,6 +495,35 @@ console.log(pigLatin("Smile"));      // "ilesmay"
 
 
 
+// Q 16
+
+// Check if the word is parindrome 
+
+
+
+function parindrome (str) {
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+
+  if (str[left] !== str[right]) {
+    return false;
+  }
+  left ++;
+  right --;
+
+  }
+
+  return true;
+
+ 
+}
+
+console.log(parindrome("wolrd"));
+console.log(parindrome("racecar"));
+
+
 
 
 
