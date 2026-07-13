5d274bc3e617f93e6a132ee5468f374a878f53b2
Aimable Ndizeye <naimable883@gmail.com>
2026-07-13 10:00:45 +0200

    Q22

--- script.js
+++ script.js
@@ -631,6 +631,31 @@ function whatday(num) {
 }
 
 
+// Q 22
+
+// Reverse a message so that the words and letters passed into it are made lower case and reversed. In addition, capitalise the first letter of the newly reversed words. If a number or symbol(!#,>) is now in the first position of the word, no capitalisation needs to occur
+
+function reverseMessage(str) {
+  return str
+    .toLowerCase()                 
+    .split(" ")                     
+    .reverse()                      
+   .map(word => {
+    let reversed = word.split("").reverse().join("");
+
+    if (reversed === "") return "";
+
+    if (/[a-z]/.test(reversed[0])) {
+        return reversed[0].toUpperCase() + reversed.slice(1);
+    }
+
+    return reversed;
+})
+    .join(" ");
+}
+
+console.log(reverseMessage('This is an example of a Reversed Message!'));
+
 
 
 
