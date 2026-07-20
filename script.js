fee2feaf352651e5b6b8fc8259d27d9eb0e63fbb
Aimable Ndizeye <naimable883@gmail.com>
2026-07-20 09:54:30 +0200

    Q27

--- script.js
+++ script.js
@@ -763,6 +763,28 @@ console.log(getCount("aimable"));
 
 
 
+// Q 27 
+
+// Trolls are attacking your comment section!
+
+// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+// Note: for this kata y isn't considered a vowel
+
+
+
+function removeVowel (str){
+   return str.replace (/[auioe]/gi, "");
+}
+
+console.log(removeVowel("aimAble"));
+
+
+
 
 
 
