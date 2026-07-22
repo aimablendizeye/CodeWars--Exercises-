4989cbd8f190458c0a9cdd3ffe3bf3599ea20ed3
Aimable Ndizeye <naimable883@gmail.com>
2026-07-22 11:49:31 +0200

    Q28

--- script.js
+++ script.js
@@ -784,6 +784,20 @@ function removeVowel (str){
 console.log(removeVowel("aimAble"));
 
 
+// Q 28;
+
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+function fakeBin(x) {
+  return x
+    .split('')
+    .map(digit => digit < 5 ? '0' : '1')
+    .join('');
+}
+
+
+
+
 
 
 
