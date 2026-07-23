83ff15e1118a841de46d3a1ce05201d728cdd438
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-07-23 16:57:59 +0200

    Q29

--- script.js
+++ script.js
@@ -797,6 +797,29 @@ function fakeBin(x) {
 
 
 
+// Q 29
+
+// Some people just have a first name; some people have first and last names and some people have first, middle and last names.
+
+// You task is to initialize the middle names (if there is any).
+
+// Examples
+// 'Jack Ryan'                   => 'Jack Ryan'
+// 'Lois Mary Lane'              => 'Lois M. Lane'
+// 'Dimitri'                     => 'Dimitri'
+// 'Alice Betty Catherine Davis' => 'Alice B. C. Davis
+
+
+  function initializeNames(name){
+   var n = name.split(" ");
+    if(n.length < 3) return name;
+     for(i = 1; i < n.length-1; i++){
+       n[i] = n[i][0] + ".";
+}
+      return n.join(" ");
+}
+
+
 
 
 
