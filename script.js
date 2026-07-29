7819c0301000e46a5b80c3627ef11f893bc81f54
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-07-29 20:05:37 +0200

    Q40

--- script.js
+++ script.js
@@ -1073,5 +1073,21 @@ function stringMerge(string1, string2, letter){
     return `${first}${second}`;
 }
 
+// Q 40 
+
+// Make a function "add" that will be able to sum elements of list continuously and return a new list of sums.
+
+// For example:
+
+// add [1,2,3,4,5] == [1, 3, 6, 10, 15], because it's calculated like 
+// this : [1, 1 + 2, 1 + 2 + 3, 1 + 2 + 3 + 4, 1 + 2 + 3 + 4 + 5]
+
+function add(arr) {
+  return arr.reduce((acc, curr) => {
+    const sum = (acc.length ? acc[acc.length - 1] : 0) + curr;
+    acc.push(sum);
+    return acc;
+  }, []);
+}
 
 
