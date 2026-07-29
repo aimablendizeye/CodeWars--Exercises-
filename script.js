201a2df6146423cb6ef8b19f5bb35187535dc735
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-07-29 10:37:11 +0200

    Q39

--- script.js
+++ script.js
@@ -1052,6 +1052,26 @@ function  calculateAge(birth, customYear) {
 
 console.log(calculateAge(2001, 2002));
 
+// Q 39
+
+// Given two words and a letter, return a single word that's a combination of both words, merged at the point where the given letter first appears in each word. The returned word should have the beginning of the first word and the ending of the second, with the dividing letter in the middle. You can assume both words will contain the dividing letter.
+
+// Examples
+// ("hello", "world", "l")       ==>  "held"
+// ("coding", "anywhere", "n")   ==>  "codinywhere"
+// ("jason", "samson", "s")      ==>  "jasamson"
+// ("wonderful", "people", "e")  ==>  "wondeople"
+
+
+function stringMerge(string1, string2, letter){
+  let index1 = string1.indexOf(letter);
+  let index2 =string2.indexOf (letter);
+  
+    let first = string1.slice(0,index1);
+    let second = string2.slice (index2 );
+
+    return `${first}${second}`;
+}
 
 
 
