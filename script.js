50d8edcd6301b6bfed6aef64ff6e40a938777269
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-07-30 17:05:23 +0200

    Q43

--- script.js
+++ script.js
@@ -1175,3 +1175,39 @@ function peopleWithAgeDrink(old) {
   }
   
 };
+
+
+// Q 43 
+
+
+// Give you two strings: s1 and s2. If they are opposite, return true; otherwise, return false. Note: The result should be a boolean value, instead of a string.
+
+// The opposite means: All letters of the two strings are the same, but the case is opposite. you can assume that the string only contains letters or it's a empty string. Also take note of the edge case - if both strings are empty then you should return false/False.
+
+// Examples (input -> output)
+// "ab","AB"     -> true
+// "aB","Ab"     -> true
+// "aBcd","AbCD" -> true
+// "AB","Ab"     -> false
+// "",""         -> false
+
+function isOpposite(s1, s2){
+  //coding here...
+  s1 = s1.split('');
+  s2 = s2.split('');
+  if(s1.length == 0 && s2.length ==0) return false
+  if(s1.length != s2.length) return false
+  
+  let receiver = [];
+  for(let i=0; i<s1.length; i++){
+    
+    if(s1[i] == s1[i].toLowerCase()){
+      
+      receiver.push(s1[i].toUpperCase() == s2[i]);
+    }
+    else if(s1[i] == s1[i].toUpperCase()){
+       receiver.push(s1[i].toLowerCase() == s2[i]); 
+    }
+  }
+  return receiver.every(item => item == true);
+}
