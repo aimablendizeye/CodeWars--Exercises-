23cde82e4efe2e90ba50370abe7235e4a2c9cdb3
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-07-30 19:10:16 +0200

    Q43

--- script.js
+++ script.js
@@ -1211,3 +1211,20 @@ function isOpposite(s1, s2){
   }
   return receiver.every(item => item == true);
 }
+
+
+// Q 43 
+
+// You will be given a string featuring a cat 'C' and a mouse 'm'. The rest of the string will be made up of '.'. The string will start with the cat, and end with the mouse.
+
+// You need to find out if the cat can catch the mouse from its current position. The cat can jump over at most three characters. So:
+
+// "C.....m" returns "Escaped!" <-- more than three characters between
+
+// "C...m" returns "Caught!" <-- as there are three characters between the two, the cat can jump
+
+
+
+function catMouse(x) {
+  return x.length <= 5 ? "Caught!" : "Escaped!";
+}
