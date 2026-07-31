65ab59c7742104f0d1bdb316087c025c32a0c55d
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-07-31 15:10:37 +0200

    Q46

--- script.js
+++ script.js
@@ -1330,3 +1330,15 @@ function HQ9(code) {
   
   // For '+', or any other character not matched above, return undefined
   return undefined;
+}
+
+// Q46 
+
+// Task
+// Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise
+
+function xor(a,b){
+    if (a === b) return false;
+    return true;
+}
+console.log(xor(false,false));
