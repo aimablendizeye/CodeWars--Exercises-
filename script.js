09eec6371ba9f21e185ebc9e37641196a2501479
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-07-31 14:41:13 +0200

    Q45

--- script.js
+++ script.js
@@ -1270,3 +1270,25 @@ function multiply(number){
    let power = 5 ** leng;
    return number * power;
 }
+
+// Q 45 
+
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+
+// Hint: Don't forget to check for bad values like null/undefined
+
+
+function countSheeps(sheep) {
+   let present = sheep.filter (n => n === true);
+    return present.length 
+}
