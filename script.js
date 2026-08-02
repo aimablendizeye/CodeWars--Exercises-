35dcbdc8cfe44b4fab3d5cc55cc7a7d23d7270bb
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-08-02 14:35:45 +0200

    Q49

--- script.js
+++ script.js
@@ -1385,3 +1385,25 @@ console.log(sum (2,3));
 function openOrSenior(data){
   return  data.map(([age,handicap]) => age>=55 && handicap > 7? "Senior":"Open")
 }
+
+
+// Q49 
+
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string (alphabetical ascending), the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+
+function longest (s1,s2){
+    let result = s1 + s2;
+    let final = [...new Set(result)].sort().join('');
+    return final;
+    
+}
+console.log(longest("aimable" , "adizeye"));
