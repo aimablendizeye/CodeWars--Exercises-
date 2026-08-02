f9ed9f333655f52d3871f44d34ddbd21c196036c
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-08-02 16:30:07 +0200

    Q51

--- script.js
+++ script.js
@@ -1424,3 +1424,22 @@ function getChar (C){
 }
 
 console.log(getChar(68));
+
+// Q 51 
+
+// The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+// But it's not working properly.
+
+// Task
+// Fix the bug so we can all go home early.
+
+// Notes
+// String str will never be null.
+
+
+var replaceDots = function(str) {
+  return str.replaceAll('.', '-');
+}
+
+
