74e06e66a59c9406f45e9ee24ad10d418d1402a2
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-08-03 15:24:06 +0200

    Q55

--- script.js
+++ script.js
@@ -1523,6 +1523,19 @@ function wordsToMarks(string){
   
 }
 
+// Q 55
+
+// The string given to your function has had an "egg" inserted directly after each consonant. You need to return the string before it became eggcoded.
+
+// Example
+// unscrambleEggs("Beggegeggineggneggeregg")  =>  "Beginner"
+// //             "B---eg---in---n---er---"
+
+
+function unscrambleEggs(word) {
+  return word.replaceAll("egg","");
+}
+
 
 
 
