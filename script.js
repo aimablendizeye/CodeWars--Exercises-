a29d08182dd4aed01f292c090233d92069c95acc
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-08-04 21:29:54 +0200

    Suggested Approach

--- script.js
+++ script.js
@@ -1538,6 +1538,32 @@ function unscrambleEggs(word) {
 
 
 
+//  56
+
+// In this kata you will be given an array of the dimensions of rectangles (array with width and length) and circles (radius - just a number). Your task is to sort the objects by their area in ascending order and return the sorted array of areas.
+
+// Examples
+//    [ [4.23, 6.43], 1.23,   3.444,  [1.342, 3.212] ] 
+// // [ rectangle,   circle,  circle,   rectangle    ]
+// --->
+// [ 4.31, 4.75, 27.2, 37.26 ]
+
+
+function sortByArea(array) {
+  return array
+    .map(shape => {
+      if (Array.isArray(shape)) {
+        return +(shape[0] * shape[1]).toFixed(2);
+      }
+
+      return +(Math.PI * shape ** 2).toFixed(2);
+    })
+    .sort((a, b) => a - b);
+  }
+
+  
+
+
 
 
 
