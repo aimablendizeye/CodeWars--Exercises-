1bb162964993eb781f07c79057defa1647bbbf1e
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-08-06 14:22:04 +0200

    Q62

--- script.js
+++ script.js
@@ -1666,6 +1666,36 @@ function letterCheck (arr){
 console.log(letterCheck(["trances", "nectar"]));
 
 
+// Q 62
+
+// Write function which takes a string and make an acronym of it.
+
+// Rule of making acronym in this kata:
+
+// split string to words by space char
+// take every first letter from word in given string
+// uppercase it
+// join them toghether
+// Eg:
+
+// Code wars -> C, w -> C W -> CW
+// Note: There will be at least two words in the given string!
+
+
+function toAcronym(inp){
+    let acron = inp.split(' ');
+    let result = ""
+     for (const char of acron){
+        result +=char[0];
+        
+     }
+     return result.toUpperCase();
+
+}
+
+console.log(toAcronym("aimable ndizeye mugabo"));
+
+
 
 
 
