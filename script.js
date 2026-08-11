f4f26350ec907b269e74e75b85dd0450c808ac74
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-08-11 17:05:44 +0200

    Q66

--- script.js
+++ script.js
@@ -1812,6 +1812,34 @@ function reverseByCenter (s){
 console.log(reverseByCenter("agent"));
 
 
+// Q 67 
+
+// Write a function that sums squares of numbers in list that may contain more lists
+// Example:
+
+// var l = [1,2,3]
+// SumSquares(l) == 14
+
+// var l = [[1,2],3]
+// SumSquares(l) == 14
+
+// var l = [[[[[[[[[1]]]]]]]]]
+// SumSquares(l) == 1
+
+// var l = [10,[[10],10],[10]]
+// SumSquares(l) == 400
+// Note: your solution must NOT modify the original list
+
+
+function  SumSquares (l){
+
+    let arr = l.flat(Infinity);
+    let squares = arr.map(n =>  n**2);
+    return squares.reduce((a,b) => a + b,0);
+    
+}
+
+console.log( SumSquares([10,[[10],10],[10]]));
 
 
 
