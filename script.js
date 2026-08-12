1aecd20fbdca41aab9c6a0535b77e74eac3ab012
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-08-12 11:39:33 +0200

    Q68

--- script.js
+++ script.js
@@ -1878,7 +1878,36 @@ function validateMessage(msg) {
   }
 
   return !/<[^>]*>/g.test(msg);
+  
 }
+
+
+// Q 68 
+
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+// If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+// More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+// Example: (input --> output)
+
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+
+
+function dna(str) {
+    return str.replace(/[ATGC]/gi, letter => {
+        if (letter.toUpperCase() === "A") return "T";
+        if (letter.toUpperCase() === "T") return "A";
+        if (letter.toUpperCase() === "G") return "C";
+        if (letter.toUpperCase() === "C") return "G";
+    });
+}
+
+console.log(dna("ATTGC"));
 
 
 
