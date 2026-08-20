b676efdb86aebe0dea7df6589e01b84833ebd46a
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-08-20 13:41:52 +0200

    Q78

--- script.js
+++ script.js
@@ -2168,6 +2168,19 @@ function arrayPacking (arr) {
   let finalArr = newArr.join('');
   
    return parseInt (finalArr,2);
+
+}
+
+
+
+// Q 78 
+
+// Find the volume of a cone whose radius and height are provided as parameters to the function volume. Use the value of PI provided by your language (for example: Math.PI in JS, math.pi in Python or Math::PI in Ruby) and round down the volume to an Interger.
+
+// If you complete this kata and there are no issues, please remember to give it a ready vote and a difficulty rating. :)
+
+function volume(r,h) {
+ return Math.floor(Math.PI * r**2 * h/3);
 }
 
 
