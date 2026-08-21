39ce8fa7bee98725f0007ef652b90d0abc24bbff
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-08-21 14:44:56 +0200

    Q79

--- script.js
+++ script.js
@@ -2184,5 +2184,20 @@ function volume(r,h) {
 }
 
 
+// Q 76 
+
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+function moveZeros (arr){
+   let zeroes = arr.filter (n => n === 0);
+   let nonZeroes = arr.filter(n => n !==0)
+    nonZeroes.push(...zeroes);
+   return nonZeroes;
+}
+
+
+
 
 
