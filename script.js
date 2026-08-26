4ea26564449471b2d76295973b617bc892323964
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-08-26 14:01:33 +0200

    Q89

--- script.js
+++ script.js
@@ -2498,3 +2498,34 @@ function order(str) {
 }
 
 
+
+// Q 89 
+
+// You get a list of integers, and you have to write a function mirror that returns the "mirror" (or symmetric) version of this list: i.e. the middle element is the greatest, then the next greatest on both sides, then the next greatest, and so on...
+
+// More info
+// The list will always consist of integers in range -1000..1000 and will vary in size between 0 and 10000. Your function should not mutate the input array, and this will be tested (where applicable). Notice that the returned list will always be of odd size, since there will always be a definitive middle element.
+
+
+function mirror (arr) {
+ 
+
+  if (arr.length === 0) return []
+
+  let large = Math.max(...arr)
+
+  let largearr = arr.filter(n => n === large)
+  largearr.shift(arr[0]);
+
+  
+   
+
+let nonMiddle = arr.filter(n => n !== large);
+ const asc = nonMiddle.map(e => e).sort((a,b)=> a - b)
+ const desc = nonMiddle.map(e => e).sort((a,b)=> b - a)
+
+ return [...asc,...largearr,large,...largearr,...desc];
+  
+}
+
+console.log(mirror([1,2,3,4,6,7]));
