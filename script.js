ea0aa954f1509782bcc0beb30048e4c1d6486b1b
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-09-01 12:32:19 +0200

    SomeOne's approach

--- script.js
+++ script.js
@@ -2791,3 +2791,17 @@ function createPhone(arr) {
 }
 
 console.log(createPhone([1,2,3,4,5,6,7,8,9,0]));
+
+
+// 
+function createPhoneNumber(numbers){
+  var format = "(xxx) xxx-xxxx";
+  
+  for(var i = 0; i < numbers.length; i++)
+  {
+    format = format.replace('x', numbers[i]);
+  }
+  
+  return format;
+}
+console.log(createPhoneNumber("1234567890"));
