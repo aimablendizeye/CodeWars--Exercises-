5d9b1c5f23b36769973e6074a067d9b1272610c6
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-09-23 18:22:47 +0200

    not yes done tobe continue tomorrow

--- 6kyu_Questions.js
+++ 6kyu_Questions.js
@@ -208,4 +208,30 @@ console.log(counting(1000,1000));
 
 
 
+// Q 6 
 
+// Find the most common letter (not a space) in the given string (comprised of at least 3 lowercase words) and replace it with the given letter.
+
+// If such letters are two or more, choose the one that appears earliest in the string.
+
+// For example:
+
+// ('my mom loves me as never did', 't') => 'ty tot loves te as never did'
+// ('real talk bro', 'n') => 'neal talk bno'
+// ('great job go ahead', 'k') => 'grekt job go khekd'
+
+
+function replacing (string, letter) {
+  let lower = string.toLowerCase().replaceAll(" ","").split('');
+ let countArr = []
+  let obj = lower.reduce((count, letter) => {
+    count[letter] = (count[letter] || 0) + 1
+    return count;
+  }, {})
+     ;
+  return obj;
+  
+  
+}
+
+console.log(replacing("my mom loves me as never did"))
